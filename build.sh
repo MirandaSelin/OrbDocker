87a33916a8d6c080175189dec90d0bee64d23a38
MirandaSelin <mirandaselin101@gmail.com>
2024-07-08 12:24:15 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,11 @@
+# UI permisions
+XSOCK=/tmp/.X11-unix
+XAUTH=/tmp/.docker.xauth
+touch $XAUTH
+xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
+
+xhost +local:docker
+
 # remove old container and image
 docker stop -t 0 mscv-slam-dev
 docker image rm -f mscv-slam-dev:0.1

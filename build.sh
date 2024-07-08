d03e4ff1f104711c4ccda497810d8c9f31682c03
MirandaSelin <mirandaselin101@gmail.com>
2024-07-08 13:37:20 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,9 +7,9 @@ xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
 xhost +local:docker
 
 # remove old container and image
-docker stop -t 0 mscv-slam-dev
-docker image rm -f mscv-slam-dev:0.1
-docker rm -f mscv-slam-dev &>/dev/null
+docker stop -t 0 mes-orb-slam
+docker image rm -f mes-orb-slam:0.1
+docker rm -f mes-orb-slam &>/dev/null
 
 # build
-docker build -t mscv-slam-dev:0.1 .
+docker build -t mes-orb-slamv:0.1 .

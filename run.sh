cdddf4d8b09385a781ff5e1d3d365ca779b5e028
MirandaSelin <mirandaselin101@gmail.com>
2024-07-10 11:28:03 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,10 @@
-# create a new container
+#!/bin/bash
+# Allow Docker container to access the X server
+xhost +local:root
+
+# Create a new container
 docker run -td --privileged --net=host --ipc=host \
     --name="mes-orb-slam" \
     -e "DISPLAY=$DISPLAY" \
     -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
-    mes-orb-slam:0.1 \
+    mes-orb-slam:0.1
